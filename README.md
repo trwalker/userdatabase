5b1fff25e3540c746324313cae7a011c16b03c59
Joe <joe@IntegrateAdmins-MacBook-Pro.local>
2016-12-16 22:42:56 -0700

    Added User Dropdown

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ TODO:
 
 - Add to profile page.
 - Build user information database.
-- Style secondary navigation.
 - Add reset password function.
 - Add change email function.
 - Add email activation.
 - Implement LESS styling.
-- Implement Material Icons.
--- js/controller-head.js
+++ js/controller-head.js
@@ -1,5 +1,5 @@
 // Primary App Controller
-app.controller('NavCtrl', function($scope, $mdToast, $document) {
+app.controller('HeadCtrl', function($scope, $mdToast, $document) {
   // Menu Item Selection
   var url = document.URL;
   var array = url.split('/');
@@ -7,7 +7,6 @@ app.controller('NavCtrl', function($scope, $mdToast, $document) {
 
   //console.log("Pathname: "+pathname);
   $scope.currentNavItem = pathname;
-  $scope.hideProfile = true;
 
   // Toasts
   var toastContent = null;
@@ -32,17 +31,38 @@ app.controller('NavCtrl', function($scope, $mdToast, $document) {
       uid = user.uid;
 
       $scope.currentNavItem = "home";
-
+      $scope.userStatus = "Welcome back, "+ displayName +"!";
       $scope.hideLogin = true;
+      $scope.hideCreate = true;
       $scope.hideProfile = false;
+      $scope.hideUpdateProfile = false;
 
       //console.log("Provider-specific UID: "+uid);
       //console.log("Email: "+email);
     } else {
       $scope.currentNavItem = "home";
-
+      $scope.userStatus = "Logged Out";
       $scope.hideLogin = false;
+      $scope.hideCreate = false;
       $scope.hideProfile = true;
+      $scope.hideUpdateProfile = true;
     }
   });
+
+  // User Menu Actions
+  $scope.loginLink = function () {
+    $scope.currentNavItem = null;
+  }
+
+  $scope.registerLink = function () {
+    $scope.currentNavItem = null;
+  }
+
+  $scope.profileLink = function () {
+    $scope.currentNavItem = null;
+  }
+
+  $scope.updateLink = function () {
+    $scope.currentNavItem = null;
+  }
 });
