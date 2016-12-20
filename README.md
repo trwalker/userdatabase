21a41f6da1cd8baa30f5a90b33194baf6eb9b99a
Joe <joeberthelot88@gmail.com>
2016-12-20 15:59:00 -0700

    Added Measurements

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ TODO:
 - Add email activation.
 - Add link to set display name from Profile page if not yet set.
 - Set contents of display name input dependent on if already set or not.
+- Add multiple 'My Body' records.
+- Add validation for input fields.
+- Create a list of users page.
--- js/controller-myBody.js
+++ js/controller-myBody.js
@@ -18,16 +18,24 @@ app.controller('BodyCtrl', function($scope, $mdToast, $filter, $timeout) {
   var date = new Date();
 
   // Add database entry.
-  $scope.updateBody = function(userId, userName, weight, currentDate) {
+  $scope.updateBody = function(userId, userName, weight, chest, waist, hips, fat, currentDate) {
 
     userId = user.uid;
     userName = user.displayName;
     weight = $scope.bodyWeight;
+    chest = $scope.bodyChest;
+    waist = $scope.bodyWaist;
+    hips = $scope.bodyHips;
+    fat = $scope.bodyFat;
     currentDate = $filter('date')(new Date(), 'MM/dd/yyyy');
 
     database.ref('users/' + userId).set({
       user: userName,
       weight: weight,
+      chest: chest,
+      waist: waist,
+      hips: hips,
+      fat: fat,
       updated: currentDate
     });
     showToast("Your information has been recorded!");
@@ -40,6 +48,10 @@ app.controller('BodyCtrl', function($scope, $mdToast, $filter, $timeout) {
     $timeout(function() {
         $scope.displayName = snapshot.val().user;
         $scope.userWeight = snapshot.val().weight;
+        $scope.userChest = snapshot.val().chest;
+        $scope.userWaist = snapshot.val().waist;
+        $scope.userHips = snapshot.val().hips;
+        $scope.userFat = snapshot.val().fat;
         $scope.updated = snapshot.val().updated;
     });
   });
